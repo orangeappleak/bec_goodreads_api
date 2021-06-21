bb066038fb12bfd4d8a08812e2b25f9ce35dde4b
orangeappleak <appannagarikarthik.123@gmail.com>
2021-06-21 18:36:55 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import jsonify
+
+main_app = Flask(__name__)
+
+
+@main_app.route('/popularBooks'):
+def popular_books():
+    #Write the code in a diff file and return
+    #the list of popular books from goodreads.com.
+    pass
+
+#similarly create other roots and get the required data
+#from goodread.
